b020ba107772ca947b079e92d1dae090a0a71dba
yeckoh <yeckoh@gmail.com>
2021-04-08 11:26:41 -0400

    add world model qc, smd

---
+++ game/modelsrc/weapons/worldmodel/wwmt_exports/shotgun_shell.bat
@@ -0,0 +1,4 @@
+@echo on
+REM Compiling using the Wall Worm Batch File...
+"L:/Program Files (x86)/Steam/steamapps/common/Source SDK Base 2013 Multiplayer/bin/studiomdl.exe" -game "L:/Program Files (x86)/Steam/steamapps/sourcemods/zombie_master_reborn" -nop4 "L:/Program Files (x86)/Steam/steamapps/sourcemods/zombie_master_reborn/modelsrc/weapons/shotgun_shell.qc"
+@pause
