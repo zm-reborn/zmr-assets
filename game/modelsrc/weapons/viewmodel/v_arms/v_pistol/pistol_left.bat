190d47f4f0e452eb2aa47ae8fe971c82074f8789
yeckoh <yeckoh@gmail.com>
2024-10-07 20:07:43 -0400

    add hand qc compile bases and bonemerger

---
+++ game/modelsrc/weapons/viewmodel/v_arms/v_pistol/pistol_left.bat
@@ -0,0 +1,4 @@
+@echo on
+REM Compiling using the Wall Worm Batch File...
+"L:/Program Files (x86)/Steam/steamapps/common/Source SDK Base 2013 Multiplayer/bin/studiomdl.exe" -game "L:/_compile/git/zmr-game/mp/game/zombie_master_reborn" -nop4 "L:/_compile/git/assets_mehisfork/game/modelsrc/weapons/viewmodel/v_arms/v_pistol/q_pistoll.qc"
+@pause
